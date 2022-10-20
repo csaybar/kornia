8fbd04a7bf7ccbc7c5bc095fa275199a20d8f016
edgar <edgar.riba@gmail.com>
2022-10-20 15:11:24 +0200

    polish tests

--- test/geometry/liegroup/test_so3.py
+++ test/geometry/liegroup/test_so3.py
@@ -3,16 +3,36 @@ import torch
 
 from kornia.geometry.liegroup.so3 import So3
 from kornia.geometry.quaternion import Quaternion
-from kornia.testing import assert_close
+from kornia.testing import BaseTester
 
 
-class TestSo3:
+class TestSo3(BaseTester):
     def test_smoke(self, device, dtype):
         q = Quaternion.from_coeffs(1.0, 0.0, 0.0, 0.0)
         q = q.to(device, dtype)
         s = So3(q)
         assert isinstance(s, So3)
-        assert_close(s.q.data, q.data)
+        self.assert_close(s.q.data, q.data)
+
+    # TODO: implement me
+    def test_cardinality(self, device, dtype):
+        pass
+
+    # TODO: implement me
+    def test_exception(self, device, dtype):
+        pass
+
+    # TODO: implement me
+    def test_gradcheck(self, device):
+        pass
+
+    # TODO: implement me
+    def test_jit(self, device, dtype):
+        pass
+
+    # TODO: implement me
+    def test_module(self, device, dtype):
+        pass
 
     @pytest.mark.parametrize("batch_size", (1, 2, 5))
     def test_init(self, device, dtype, batch_size):
@@ -21,7 +41,7 @@ class TestSo3:
         s1 = So3(q)
         s2 = So3(s1.q)
         assert isinstance(s2, So3)
-        assert_close(s1.q.data, s2.q.data)
+        self.assert_close(s1.q.data, s2.q.data)
 
     @pytest.mark.parametrize("batch_size", (1, 2, 5))
     def test_getitem(self, device, dtype, batch_size):
@@ -30,7 +50,7 @@ class TestSo3:
         s = So3(q)
         for i in range(batch_size):
             s1 = s[i]
-            assert_close(s1.q.data[0], q.data[i])
+            self.assert_close(s1.q.data[0], q.data[i])
 
     @pytest.mark.parametrize("batch_size", (1, 2, 5))
     def test_exp(self, device, dtype, batch_size):
@@ -38,7 +58,7 @@ class TestSo3:
         q = q.to(device, dtype)
         s = So3(q)
         zero_vec = torch.zeros((batch_size, 3))
-        assert_close(s.exp(zero_vec).q.data, q.data)  # exp of zero vec is identity
+        self.assert_close(s.exp(zero_vec).q.data, q.data)  # exp of zero vec is identity
 
     @pytest.mark.parametrize("batch_size", (1, 2, 5))
     def test_log(self, device, dtype, batch_size):
@@ -46,7 +66,7 @@ class TestSo3:
         q = q.to(device, dtype)
         s = So3(q)
         zero_vec = torch.zeros((batch_size, 3))
-        assert_close(s.log(), zero_vec)  # log of identity quat is zero vec
+        self.assert_close(s.log(), zero_vec)  # log of identity quat is zero vec
 
     @pytest.mark.parametrize("batch_size", (1, 2, 5))
     def test_exp_log(self, device, dtype, batch_size):
@@ -55,13 +75,13 @@ class TestSo3:
         s = So3(q)
         a = torch.rand(batch_size, 3, device=device, dtype=dtype)
         b = s.exp(a).log()
-        assert_close(b, a)
+        self.assert_close(b, a)
 
     @pytest.mark.parametrize("batch_size", (1, 2, 5))
     def test_hat(self, device, dtype, batch_size):
         v = torch.tensor([1, 2, 3]).repeat(batch_size, 1)
         v = v.to(device, dtype)
-        assert_close(So3.hat(v).unique()[-3:], v[0, :])
+        self.assert_close(So3.hat(v).unique()[-3:], v[0, :])
 
     @pytest.mark.parametrize("batch_size", (1, 2, 5))
     def test_vee(self, device, dtype, batch_size):
@@ -69,14 +89,14 @@ class TestSo3:
         omega = omega.to(device, dtype)
         expected = torch.tensor([[8, 3, 4]]).repeat(batch_size, 1)
         expected = expected.to(device, dtype)
-        assert_close(So3.vee(omega), expected)
+        self.assert_close(So3.vee(omega), expected)
 
     @pytest.mark.parametrize("batch_size", (1, 2, 5))
     def test_hat_vee(self, device, dtype, batch_size):
         a = torch.rand(batch_size, 3, device=device, dtype=dtype)
         omega = So3.hat(a)
         b = So3.vee(omega)
-        assert_close(b, a)
+        self.assert_close(b, a)
 
     @pytest.mark.parametrize("batch_size", (1, 2, 5))
     def test_matrix(self, device, dtype, batch_size):
@@ -91,10 +111,10 @@ class TestSo3:
             pquat = Quaternion(torch.cat([torch.tensor([0]), pvec])[None, :])
             qp_ = q1 * pquat * q1.inv()
             rp_ = torch.matmul(r1, pvec)[None, :]
-            assert_close(rp_, qp_.vec)  # p_ = R*p = q*p*q_inv
+            self.assert_close(rp_, qp_.vec)  # p_ = R*p = q*p*q_inv
 
     @pytest.mark.parametrize("batch_size", (1, 2, 5))
     def test_inverse(self, device, dtype, batch_size):
         q = Quaternion.random(batch_size)
         q = q.to(device, dtype)
-        assert_close(So3(q).inverse().inverse().q.data, q.data)
+        self.assert_close(So3(q).inverse().inverse().q.data, q.data)
